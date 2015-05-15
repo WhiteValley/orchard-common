f357fd5fca1186b4e372bdaab0a6af53296f8592
David Glenn <david@whitevalleysoftware.com>
2015-05-15 13:12:46 +0100

    Add AutorouteFilter to allow queries based on URL
    Adds a Query filter based on the URL of the Autoroute Permalink

---
+++ Filters/AutorouteFilter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Orchard.Autoroute.Models;
+using Orchard.ContentManagement;
+using Orchard.DisplayManagement;
+using Orchard.Forms.Services;
+using Orchard.Localization;
+using Orchard.Projections.Descriptors.Filter;
+using Orchard.Projections.Services;
+
+
+namespace WhiteValley.Common.Filters
+{
+    public class AutorouteFilter : IFilterProvider
+    {
+
+        public Localizer T { get; set; }
+
+
+
+        public AutorouteFilter()
+        {
+            T = NullLocalizer.Instance;
+        }
+        
+
+        public void Describe(DescribeFilterContext describe)
+        {
+            describe.For("Autoroute", T("Autoroute"), T("The ID of the area"))
+                .Element("Permalink", T("Permalink"), T("The URL for the content"),
+                    ApplyFilter,
+                    DisplayFilter,
+                    "AutorouteFilter"
+                );
+
+        }
+
+
+        public void ApplyFilter(FilterContext context)
+        {
+            var path = (string)context.State.Path;
+            if (String.IsNullOrEmpty(path))
+                return;
+
+            context.Query = context.Query.Where(alias => alias.ContentPartRecord<AutoroutePartRecord>(), expr => expr.Like("DisplayAlias", path, HqlMatchMode.Start));
+        }
+
+
+        public LocalizedString DisplayFilter(FilterContext context)
+        {
+            string url = context.State.Path;
+            return String.IsNullOrEmpty(url) ? T("Starts with URL") : T("Starts with URL {0}", url);
+        }
+
+    }
+
+}
---
+++ Filters/AutorouteFilterForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Orchard.DisplayManagement;
+using Orchard.Forms.Services;
+using Orchard.Localization;
+
+
+namespace WhiteValley.Common.Filters
+{
+
+    public class AutorouteFilterForm : IFormProvider
+    {
+        protected dynamic Shape { get; set; }
+        public Localizer T { get; set; }
+
+
+        public AutorouteFilterForm(IShapeFactory shapeFactory)
+        {
+            Shape = shapeFactory;
+            T = NullLocalizer.Instance;
+        }
+
+
+        public void Describe(DescribeContext context)
+        {
+            Func<IShapeFactory, object> form =
+                shape =>
+                {
+                    var s = Shape.Form(
+                        Id: "StartsWithPath",
+                          _Parts: Shape.Textbox(
+                            Id: "path",
+                            Name: "Path",
+                            Title: T("Path"),
+                            Description: T("Enter the path that the URL must start with")
+                            )
+                        );
+                    return s;
+                };
+            context.Form("AutorouteFilter", form);
+        }
+
+    }
+
+}
